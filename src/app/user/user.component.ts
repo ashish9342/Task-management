effa279c2ce1b418f23a493850f22617417bd309
Ashish Singh Rawat <ashish9342@gmail.com>
2024-11-05 14:32:26 +0400

    Angular 18 signal feature implemented

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users'
+import { Component, computed, input, Input } from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,21 +10,35 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+
+  // decorater approach
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}  ) name!: string;
+
+  /** ANGULAR 18 feature | signal approach */
+  // <img [src]="imagePath()" [alt]="name()"/> 
+  // this. set()  signals does not work
+  // read only property
+  avatar = input<string>();
+  name = input<string>();
+  imagePath = computed(() => {return 'assets/users/' + this.avatar()})
 
   /** use imagePath instaead of calling a function
    * getter doesnot work with signals
    * instead use computed from angular core */ 
   // get imagePath() {
-  //   console.log(this.selectedUser)
-  //   return 'assets/users/' + this.selectedUser.avatar;
+  //   // console.log(this.selectedUser)
+  //   // return 'assets/users/' + this.selectedUser.avatar;
+  //   return 'assets/users/' + this.avatar;
   // }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
     // this.selectedUser = DUMMY_USERS[randomIndex]
   }
 }
