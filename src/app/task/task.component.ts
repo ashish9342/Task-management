be6d40aad3292847815e6e86549e429ef7e2fb33
Ashish Singh Rawat <ashish9342@gmail.com>
2024-11-05 23:12:25 +0400

    feat(model) model creadted | code clean up

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { Task } from './task.model';
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -7,5 +7,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
+
 export class TaskComponent {
+  @Input({ required: true }) task!: Task
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,6 @@
 import { Component, computed, EventEmitter, input, Input, Output, output } from '@angular/core';
+import { User } from './user.model'; 
 // import { DUMMY_USERS } from '../dummy-users';
-
-// type User = {
-//   id: string,
-//   avatar: string,
-//   name: string
-// };
-
-interface User {
-  id: string,
-  avatar: string,
-  name: string
-};
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
